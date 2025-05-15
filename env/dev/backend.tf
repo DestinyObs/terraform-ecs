0104eea9dd06a676764ab3024fc609e0e6f2a5e7
Destinyobs <destinyobueh14@gmail.com>
2025-05-15 16:09:29 +0100

    configured VPC private public subnet while saving statefiles in s3

--- env/dev/backend.tf
+++ env/dev/backend.tf
@@ -1,7 +1,7 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "terraform-state-bucket-dev"  # Must match the created bucket
-#     key    = "terraform/dev/terraform.tfstate"
-#     region = "us-east-1"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-bucket-dev" 
+    key    = "terraform/dev/terraform.tfstate"
+    region = "us-east-1" 
+  }
+}
--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,7 @@ output "public_subnet_ids" {
 output "private_subnet_ids" {
   value = module.vpc.private_subnet_ids
 }
+
+output "s3_bucket_arn" {
+  value = aws_s3_bucket.terraform_state.arn
+}
