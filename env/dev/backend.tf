d396ccfcdcafac65a674a7e4642b88377d924600
Destinyobs <destinyobueh14@gmail.com>
2025-05-15 17:19:15 +0100

    fix s3

--- env/dev/backend.tf
+++ env/dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-bucket-dev" 
-    key    = "terraform/dev/terraform.tfstate"
+    bucket = "xyz-ecs-terraform-state-bucket-dev" 
+    key    = "env/dev/terraform.tfstate"
     region = "us-east-1" 
   }
 }
